ada469190806d3e4169ae7ff72d3d5595398637b
Paul Simk <paulius.simkevicius@mediamonks.com>
2019-04-09 10:55:07 +0200

    use spawn instead of exec for opening editors

--- src/async.ts
+++ src/async.ts
@@ -1,6 +1,6 @@
 import * as cp from "child_process";
 
-const exec = (command: string) =>
+const exec = (command: string): Promise<string> =>
   new Promise((resolve, reject) => {
     cp.exec(command, (err, stdout, stderr) => {
       if (err) return reject(err);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import async from "./async";
 import { Command } from "commander";
 import * as packageJson from "../package.json";
+import * as cp from "child_process";
 import { v4 as uuid } from "uuid";
 import chalk from "chalk";
+import { tmpdir } from "os";
 
 const COMMANDS = {
   check: "git --version",
@@ -10,14 +12,15 @@ const COMMANDS = {
 };
 
 const GITHUB_WEBSITE = "https://github.com";
-const TEMP_DIR = `${"${TMPDIR}"}${uuid()}`;
+const TEMP_DIR = `${tmpdir}${uuid()}`;
+const EDITOR = `$EDITOR`;
 
 const logger = (message: string, type: "log" | "info" | "error") => (
   ...args: any[]
 ) => console[type](...args, message);
 
 (async () => {
-  let repoName;
+  let repoName: string;
 
   new Command(packageJson.name)
     .version(packageJson.version)
@@ -35,13 +38,18 @@ const logger = (message: string, type: "log" | "info" | "error") => (
     logger(err, "error")();
   }
 
-  let command = `${COMMANDS.clone} ${GITHUB_WEBSITE}/${repoName} ${TEMP_DIR}`;
+  repoName = repoName.trim();
+
+  const command = `${COMMANDS.clone} ${GITHUB_WEBSITE}/${repoName} ${TEMP_DIR}`;
+  let editor = EDITOR;
+
   try {
     await async.exec(command);
-    command = `$EDITOR ${TEMP_DIR}`;
-    await async.exec(command);
+    editor = (await async.exec(`echo ${editor}`)).trim();
   } catch (err) {
     logger(`unable to execute ${command}`, "error")();
     logger(err, "error")();
   }
+
+  cp.spawn(editor, [TEMP_DIR], { stdio: "inherit" });
 })();
